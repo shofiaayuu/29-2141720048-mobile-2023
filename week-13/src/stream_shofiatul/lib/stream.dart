3fbfa9e6ef9176ce90bdfb7c8c95900fdaecf49c
Shofiatul Ayu Anikaningrum <shofiaaayuu@gmail.com>
2023-11-21 14:17:32 +0700

    W13: Jawaban Soal 2

---
+++ week-13/src/stream_shofiatul/lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.green,
+    Colors.pinkAccent,
+    Colors.red,
+    Colors.deepOrange,
+    Colors.brown,
+  ];
+}
