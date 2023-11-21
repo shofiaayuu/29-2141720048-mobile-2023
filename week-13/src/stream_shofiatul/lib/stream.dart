3df382ee2214b6992645dbb166c9362154508cef
Shofiatul Ayu Anikaningrum <shofiaaayuu@gmail.com>
2023-11-21 14:29:10 +0700

    W13: Jawaban Soal 3

--- week-13/src/stream_shofiatul/lib/stream.dart
+++ week-13/src/stream_shofiatul/lib/stream.dart
@@ -13,4 +13,12 @@ class ColorStream {
     Colors.deepOrange,
     Colors.brown,
   ];
+
+  Stream<Color> getColors() async* {
+    yield* Stream.periodic(
+      const Duration(seconds: 1), (int t) {
+        int index = t % colors.length;
+        return colors[index];
+    });
+  }
 }
