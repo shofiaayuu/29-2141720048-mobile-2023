033d7dd5e5027ce16f41a372926bd8745c809687
Shofiatul Ayu Anikaningrum <shofiaaayuu@gmail.com>
2023-11-22 20:59:45 +0700

    W13: Jawaban Soal 8

--- week-13/src/stream_shofiatul/lib/main.dart
+++ week-13/src/stream_shofiatul/lib/main.dart
@@ -37,6 +37,8 @@ class _StreamHomePageState extends State<StreamHomePage> {
   late StreamController numberStreamController;
   late NumberStream numberStream;
 
+  late StreamTransformer transformer;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,13 +80,33 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void initState() {
+    super.initState();
+     transformer = StreamTransformer<int, int>.fromHandlers(
+        handleData: (value, sink) {
+          sink.add(value * 10);
+        },
+        handleError: (error, trace, sink) {
+          sink.add(-1);
+        },
+        handleDone: (sink) => sink.close());
     // super.initState();
     // colorStream = ColorStream();
     // changeColor();
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+    // stream.listen((event) {
+    //   setState(() {
+    //     lastNumber = event;
+    //   });
+    // }).onError((error) {
+    //   setState(() {
+    //     lastNumber = -1;
+    //   });
+    // });
+    // super.initState();
+
+    stream.transform(transformer).listen((event) {
       setState(() {
         lastNumber = event;
       });
@@ -93,7 +115,9 @@ class _StreamHomePageState extends State<StreamHomePage> {
         lastNumber = -1;
       });
     });
-    super.initState();
+    
+
+   
   }
 
   @override
