f5d41855859072a158490b6eef9a109cf4df96e9
Shofiatul Ayu Anikaningrum <shofiaaayuu@gmail.com>
2023-11-23 09:14:34 +0700

    W13: Jawaban Soal 10, 11

--- week-13/src/stream_shofiatul/lib/main.dart
+++ week-13/src/stream_shofiatul/lib/main.dart
@@ -39,6 +39,8 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   late StreamTransformer transformer;
   late StreamSubscription subscription;
+  late StreamSubscription subscription2;
+  String values = '';
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +57,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Text(values),
               Text(lastNumber.toString()),
               ElevatedButton(
                   onPressed: () => addRandomNumber(),
@@ -97,19 +100,30 @@ class _StreamHomePageState extends State<StreamHomePage> {
     // changeColor();
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
-    Stream stream = numberStreamController.stream;
+    Stream stream = numberStreamController.stream.asBroadcastStream();
+    // subscription = stream.listen((event) {
+    //   // stream.listen((event) {
+    //   setState(() {
+    //     lastNumber = event;
+    //   });
+    //   // }).onError((error) {
+    //   //   setState(() {
+    //   //     lastNumber = -1;
+    //   //   });
+    //   // });
+    // });
+    // super.initState();
+
     subscription = stream.listen((event) {
-      // stream.listen((event) {
-        setState(() {
-          lastNumber = event;
-        });
-      // }).onError((error) {
-      //   setState(() {
-      //     lastNumber = -1;
-      //   });
-      // });
+      setState(() {
+        values += '$event - ';
+      });
     });
-    super.initState();
+    subscription2 = stream.listen((event) {
+      setState(() {
+        values += '$event - ';
+      });
+    });
 
     subscription.onError((error) {
       setState(() {
@@ -131,30 +145,29 @@ class _StreamHomePageState extends State<StreamHomePage> {
     //     });
     //   });
     // }
- 
   }
 
   void stopStream() {
-      numberStreamController.close();
+    numberStreamController.close();
   }
-  
+
   void addRandomNumber() {
-      Random random = Random();
-      int myNum = random.nextInt(10);
-      if (!numberStreamController.isClosed) {
-        numberStream.addNumberToSink(myNum);
-      } else {
-        setState(() {
-          lastNumber = -1;
-        });
-      }
-      // numberStream.addError();
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    if (!numberStreamController.isClosed) {
+      numberStream.addNumberToSink(myNum);
+    } else {
+      setState(() {
+        lastNumber = -1;
+      });
+    }
+    // numberStream.addError();
   }
 
   @override
   void dispose() {
-      numberStreamController.close();
-      subscription.cancel();
-      super.dispose();
+    numberStreamController.close();
+    subscription.cancel();
+    super.dispose();
   }
 }
